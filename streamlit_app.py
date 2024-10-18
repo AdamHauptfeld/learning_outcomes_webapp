5e04f81bbc72849ac83a75f25b625582688db615
AdamHauptfeld <172135239+AdamHauptfeld@users.noreply.github.com>
2024-10-17 22:33:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def main():
             (axis=1)
 
         #Display happy image
-        st.image(it_works.jpg)
+        st.image(https://github.com/AdamHauptfeld/learning_outcomes_webapp/blob/master/it_works.jpg)
         
         # Display the combined dataframe
         st.subheader("Combined Table")
